9cc729cb11ae0fafb66b70051180d323ebbb151c
Mr.BobLee <asdleemrboblee5202@gmail.com>
2026-01-06 16:29:47 +0800

    update

--- generator/template/src/mock/menu.js
+++ generator/template/src/mock/menu.js
@@ -1,1 +1,18 @@
-//
+const menuList = [
+  // 範例
+  // {
+  //   id: 1,
+  //   superuser_only: false,
+  //   title: '頁籤標題',
+  //   path: '/檔案路徑',
+  //   icon: 'el-icon-bangzhu',
+  //   menu_type: 2,
+  //   open_type: 1,
+  //   parent_id: 0,
+  //   component: '{"name":"檔案名稱","path":"檔案路徑"}',
+  //   app: '專案名稱',
+  // },
+]
+
+export default menuList
+// console.log(JSON.stringify(menuList))
